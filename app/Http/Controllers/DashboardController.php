95ab3c21cc27e2a14fdea411dbc53cbdb5786071
diky-satria <dikysr123456@gmail.com>
2021-09-01 02:10:03 +0700

    rs

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -40,7 +40,7 @@ class DashboardController extends Controller
         $qty = [];
         foreach($chartDoughnut as $cd){
             $judul[] = $cd->judul;
-            $qty[] = $cd->jumlahPinjam;
+            $qty[] = $cd->qty;
         }
 
         return response()->json([
