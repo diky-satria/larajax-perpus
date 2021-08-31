80a0c50ebed4c0d3ba1aae4a215d2c3771161ed9
diky-satria <dikysr123456@gmail.com>
2021-09-01 00:36:01 +0700

    memindahkan link cdn chart js dari template1 ke dashboard

--- resources/views/templates/template1.blade.php
+++ resources/views/templates/template1.blade.php
@@ -46,7 +46,6 @@
         </script>
     <!-- jquery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.5.1/chart.min.js"></script>
     <!-- sweetalert -->
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
